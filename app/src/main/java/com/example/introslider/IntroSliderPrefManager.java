57b51959026567b69a16cc80ce1df4c162ab8e5c
AliHejazi1382 <mrhjz08@gmail.com>
2020-10-29 18:48:29 +0330

    IntroSliderPrefManager class completed

--- app/src/main/java/com/example/introslider/IntroSliderPrefManager.java
+++ app/src/main/java/com/example/introslider/IntroSliderPrefManager.java
@@ -1,4 +1,23 @@
 package com.example.introslider;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class IntroSliderPrefManager {
+    private Context context;
+    private SharedPreferences preferences;
+    private static final String KEY_NAME = "intro";
+    private static final String KEY_VALUE = "seen";
+    public IntroSliderPrefManager(Context context) {
+        this.context = context;
+        this.preferences = context.getSharedPreferences(KEY_NAME, Context.MODE_PRIVATE);
+    }
+
+    public void setPref(boolean seen) {
+        preferences.edit().putBoolean(KEY_VALUE, seen);
+    }
+
+    public boolean getPref() {
+        return preferences.getBoolean(KEY_VALUE, true);
+    }
 }
